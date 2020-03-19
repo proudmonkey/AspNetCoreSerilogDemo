5600505448d96e9d876b67d9745b8df55b7e9888
Vincent Maverick S Durano <vmsdurano@gmail.com>
2020-03-18 19:20:43 -0500

    first commit

---
+++ AspNetCoreSerilogDemo/TestLogApi.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace AspNetCoreSerilogDemo
+{
+    public class TestLogApi
+    {
+        private readonly ILogger<TestLogApi> _logger;
+        public TestLogApi(ILogger<TestLogApi> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task DumpLogs(HttpContext context)
+        {
+            _logger.LogInformation("This is a log with INFORMATION severity level.");
+            _logger.LogWarning("This is a log with WARNING severity level.");
+            _logger.LogError("This is a log with ERROR severity level.");
+            _logger.LogCritical("This is a log with CRITICAL severity level.");
+
+            await context.Response.WriteAsync("Events has been logged.");
+        }
+    }
+}
